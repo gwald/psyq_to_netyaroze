da81c4e6463ef7e90fd224f1f27b129d3c3274ad
gwald <1996mgf@gmail.com>
2024-01-05 18:23:06 +1100

    Create ny_psyq.h

---
+++ include/ny_psyq.h
@@ -0,0 +1,38 @@
+
+#ifndef _NET_YAROZE_TO_PSYQ_GLUE_
+#define _NET_YAROZE_TO_PSYQ_GLUE_
+
+/*
+ * Prototypes
+ */
+
+#if defined(_LANGUAGE_C_PLUS_PLUS)||defined(__cplusplus)||defined(c_plusplus)
+extern  "C" {
+#endif
+
+
+// from #include <libapi.h>
+ long InitPAD(char *,long ,char *,long);
+ long StartPAD(void);
+
+#if defined(_LANGUAGE_C_PLUS_PLUS)||defined(__cplusplus)||defined(c_plusplus)
+} //extern  "C"
+#endif
+
+// FntLoad & GetPadBuf must be called AFTER GsInitGraph
+// Net Yaroze only uses the first 8 bytes, no multitap etc
+#define MAX_CONTROLLER_BYTES     8      /*  34 Size of the biggest packet  */
+                                        /* You can possible have. From PSXDEV PSYQ ctrller.h       */
+inline void GetPadBuf(volatile unsigned char **pad0, volatile unsigned char **pad1)
+{
+    static unsigned char pad_buff[2][MAX_CONTROLLER_BYTES];
+	InitPAD ((char *)pad_buff[0], MAX_CONTROLLER_BYTES, (char *)pad_buff[1], MAX_CONTROLLER_BYTES);
+	
+	StartPAD(); //Explanation - Triggered by the interruption of a vertical retrace line, this function starts to read the controller. ChangeClearPAD (1) is executed internally.
+	*pad0 = pad_buff[0];
+	*pad1 = pad_buff[1];
+
+}
+#undef MAX_CONTROLLER_BYTES
+#undef _NET_YAROZE_TO_PSYQ_GLUE_
+#endif // #ifndef _NET_YAROZE_TO_PSYQ_GLUE_
